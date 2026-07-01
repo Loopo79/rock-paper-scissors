f8d83fd5207859e844b06da234acb2c0005685cf
Loopo79 <155375560+Loopo79@users.noreply.github.com>
2026-07-02 02:09:17 +0530

    Remove global variables

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-const ROCK = 0;
-const PAPER = 1;
-const SCISSORS = 2;
-const OPTIONS = ["rock", "paper", "scissors"];
-
 function getComputerChoice() {
   const result = Math.floor(Math.random() * 3 + 1);
-  if (result === ROCK) return "rock";
-  if (result === PAPER) return "paper";
-  if (result === SCISSORS) return "scissors";
+  if (result === 0) return "rock";
+  if (result === 1) return "paper";
+  if (result === 2) return "scissors";
 }
 
 function getPlayerChoice() {
   let result = prompt("Enter rock, paper, or scissors:");
   result = result.toLowerCase();
+  const OPTIONS = ["rock", "paper", "scissors"];
   if (OPTIONS.includes(result)) return result;
   return getPlayerChoice();
 }
