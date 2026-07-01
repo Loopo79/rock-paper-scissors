753f44ee208d8532867f226384f2fd18143b2e70
Loopo79 <155375560+Loopo79@users.noreply.github.com>
2026-07-02 02:05:40 +0530

    Add player input fuctionality using prompt()

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
+const OPTIONS = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   const result = Math.floor(Math.random() * 3 + 1);
   if (result === ROCK) return "rock";
   if (result === PAPER) return "paper";
   if (result === SCISSORS) return "scissors";
+}
+
+function getPlayerChoice() {
+  let result = prompt("Enter rock, paper, or scissors:");
+  result = result.toLowerCase();
+  if (OPTIONS.includes(result)) return result;
+  return getPlayerChoice();
 }
